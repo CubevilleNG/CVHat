f873f992cf3b938ff9ecffa9b74d0cf2dba7fb98
Rob303 <robbie.kulesa@gmail.com>
2017-03-10 18:55:55 -0500

    CVHat

---
+++ src/main/java/org/cubeville/cvhat/CVHat.java
@@ -0,0 +1,44 @@
+package org.cubeville.cvhat;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CVHat extends JavaPlugin {
+
+    List<String> allowedBlocks;
+
+    @Override
+    public void onEnable() {
+        allowedBlocks = getConfig().getStringList("blocks");
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = (Player) sender;
+
+        if (label.equalsIgnoreCase("hat")) {
+            PlayerInventory inventory = player.getInventory();
+            ItemStack itemInHand = inventory.getItemInMainHand();
+            ItemStack itemInHelmet = inventory.getHelmet();
+            if (itemInHand.getAmount() == 1) {
+                if (player.hasPermission("cvranks.hat.any") || allowedBlocks.contains(itemInHand.getType() + "")) {
+                    inventory.setHelmet(itemInHand);
+                    inventory.setItemInMainHand(itemInHelmet);
+                } else {
+                    player.sendMessage(ChatColor.RED + "You can't put that on your head.");
+                }
+            } else {
+                player.sendMessage(ChatColor.RED + "Please only hold 1 item!");
+            }
+            return true;
+        }
+        return false;
+    }
+}
